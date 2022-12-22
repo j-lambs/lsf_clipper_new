c7611ecbda6df6f92447a4bb5cbfb4eadfa3184b
j-lambs <jambis187@gmail.com>
2022-12-21 20:05:02 -0800

    working on detect valid clip. added exec timer

--- clipper.py
+++ clipper.py
@@ -1,5 +1,19 @@
+import time
+start_time = time.time()
+
 import requests
 import re
+import urllib.request
+
+# reddit api info
+CLIENT_ID = "***REMOVED***"
+SECRET_KEY = "***REMOVED***"
+auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+data = {
+	"grant_type" : "password",
+	"username" : "***REMOVED***",
+	"password" : "***REMOVED***"
+}
 
 # gets a list of strings of the twitch clips
 def getListOfClips(page_html):
@@ -11,22 +25,21 @@ def getListOfClips(page_html):
 			twitch_only_list.append(x)
 	return twitch_only_list
 
-# reddit api info
-CLIENT_ID = "***REMOVED***"
-SECRET_KEY = "***REMOVED***"
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-data = {
-	"grant_type" : "password",
-	"username" : "***REMOVED***",
-	"password" : "***REMOVED***"
-}
+
 #GETS REDDIT JSON TEXT
-headers = {"User-Agent" : "***REMOVED***"}
-res = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
-TOKEN = res.json()["access_token"]
-headers["Authorization"] = f"bearer{TOKEN}"
-res = requests.get("https://oauth.reddit.com/r/livestreamfail/hot/", headers=headers, params={'limit' : '100'})
-lsf_page_html = res.text 
+def getRedditJSONText():
+    headers = {"User-Agent" : "***REMOVED***"}
+    res = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
+    TOKEN = res.json()["access_token"]
+    headers["Authorization"] = f"bearer{TOKEN}"
+    res = requests.get("https://www.reddit.com/r/LivestreamFail/top/?sort=top&t=day", headers=headers, params={'limit' : '100'})
+    return res.text
+ 
 
 # main function
-print(getListOfClips(lsf_page_html))
+# print(getListOfClips(getRedditJSONText()))
+request = urllib.request.urlopen(url="https://clips.twitch.tv/GrossWonderfulElephantDuDudu",timeout=3)
+# resp = urllib.request.urlopen(request, timeout=3)
+print(request.geturl())
+
+print("Process finished --- %s seconds ---" % (time.time() - start_time))
